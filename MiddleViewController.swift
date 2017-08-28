90b5915e0331382969ca6ffed1ed49ebf420f92f
user128830 <user128830@ny212.local>
2017-08-28 08:57:01 -0700

    just finished first 2 items of the STEP 3 section

---
+++ MiddleViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MiddleViewController.swift
+//  OrderOfEvents
+//
+//  Created by user128830 on 8/28/17.
+//  Copyright © 2017 user128830. All rights reserved.
+//
+
+import UIKit
+
+class MiddleViewController: UIViewController {
+
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    var eventNumber: Int = 1
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
---
+++ OrderOfEvents/LastViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LastViewController.swift
+//  OrderOfEvents
+//
+//  Created by user128830 on 8/28/17.
+//  Copyright © 2017 user128830. All rights reserved.
+//
+
+import UIKit
+
+class LastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
